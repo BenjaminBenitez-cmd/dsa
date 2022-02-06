f575419521de0d2042173bbbe74d51a7d628e584
BenjaminBenitez-cmd <benitezbenjamin853@gmail.com>
2022-02-05 22:51:47 -0600

    implemented queue using different ends of list

--- linear-ds/queue/queue.py
+++ linear-ds/queue/queue.py
@@ -1,3 +1,5 @@
+
+
 class Queue:
     def __init__(self):
         self.items = []
@@ -14,9 +16,27 @@ class Queue:
     def size(self):
         return len(self.items)
 
+# Queue implementing the front at the start of the list and the -
+# rear at the end of the list
 
-q = Queue()
+
+class QueueReversed(Queue):
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+
+# q = Queue()
+# q.enqueue(4)
+# q.enqueue('Dog')
+# q.enqueue(13)
+# print(q.size())
+
+q = QueueReversed()
 q.enqueue(4)
-q.enqueue('Dog')
-q.enqueue(13)
+q.enqueue('test')
+q.dequeue()
 print(q.size())
