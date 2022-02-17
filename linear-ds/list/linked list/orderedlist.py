aeeec14f6a25748ef2d4fa63db9fe416fb4be8e5
BenjaminBenitez-cmd <benitezbenjamin853@gmail.com>
2022-02-17 15:30:05 -0600

    finished linkedlist orderered/unordered section

---
+++ linear-ds/list/linked list/orderedlist.py
@@ -0,0 +1,113 @@
+from locale import currency
+from numpy import true_divide
+
+
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            if item == self.tail.getData():
+                self.tail = previous
+
+            previous.setNext(current.getNext())
+
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
+
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def __str__(self):
+        current = self.head
+        strArr = ""
+        while current != None:
+            strArr = strArr + " ," + str(current.getData())
+            current = current.getNext()
+        return strArr
+
+
+mylist = OrderedList()
+
+
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+mylist.pop()
+print(mylist)
